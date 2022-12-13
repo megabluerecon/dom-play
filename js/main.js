1dfaa4f2318c800d3b5fb0bd9e7a9c1e7bf03a99
dien nguyen <megabluerecon@gmail.com>
2022-12-12 16:49:57 -0800

    DOM Play 1.0

--- js/main.js
+++ js/main.js
@@ -1,3 +1,58 @@
+
+var juliet = document.getElementById('juliet-img');
+juliet.style.opacity = "0.2";
+var romeo = document.getElementById('romeo-img');
+romeo.style.opacity = "0.2";
+
+function highlight(el) {
+
+    if(el.style.backgroundColor == 'white') { //if white, turn yellow
+        el.style.backgroundColor='yellow';
+    } else { //else turn white
+        el.style.backgroundColor="white";
+    }
+}
+
+function myAlert() {
+    alert("I'm clicked!!");
+}
+
+function highlightActor(actor) {
+    //alert(actor);
+    
+    let spans = document.querySelectorAll("#play span");
+    for(const mySpan of spans) {
+        if(actor == mySpan.dataset.actor) { //if white, turn yellow
+            mySpan.style.backgroundColor='orange';
+            if(mySpan.dataset.actor == "juliet") {
+                juliet.style.opacity = "1";
+                romeo.style.opacity = "0.2";
+            } else {
+                romeo.style.opacity = "1";
+                juliet.style.opacity = "0.2";
+            }
+        } else { //else turn white
+            mySpan.style.backgroundColor="white";
+        }
+    }
+}
+
+let spans = document.querySelectorAll("#play span");
+ 
+console.log(spans);
+
+for(const mySpan of spans) {
+    //mySpan.addEventListener("click", myAlert);
+    //alert(mySpan.dataset.actor);
+
+    mySpan.addEventListener("click", function(ev){
+        highlightActor(mySpan.dataset.actor);
+
+    });
+}
+
+
+
 //https://tinyurl.com/dynamic-html-checker
 document.getElementById("html-checker").setAttribute("href","https://validator.w3.org/nu/?doc=" + location.href);
       
